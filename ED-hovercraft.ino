c6839db07833e2c21b25f58539bb101829bfe034
Coolaswa <mfavier@live.nl>
2016-10-21 18:09:03 +0200

    Code for lifting off, thrust and steering

--- ED-hovercraft.ino
+++ ED-hovercraft.ino
@@ -1,10 +1,13 @@
 #include <Servo.h>
 
 int thrust;
+int steer;
+int lift;
 Servo liftMotor;
 Servo thrustMotor;
 Servo gimbleB;
 Servo gimbleU;
+Servo steering;
 
 int gimbleUPosX = 90;
 int gimbleUPosY = 90;
@@ -14,8 +17,16 @@ int currentsystem = 0;
 
 void setup() {
   thrust = 0;
-  liftMotor.attach(0, 1000, 2000);
-  liftMotor.write(thrust);
+  thrustMotor.attach(0, 1000, 2000);
+  thrustMotor.write(thrust);
+
+  steer = 90;
+  steering.attach(1, 750, 2250);
+  steering.write(90);
+
+  lift = 0;
+  liftMotor.attach(2, 1000, 2000);
+  liftMotor.write(lift);
   
   //servoX.attach(servoPinX, 1000, 2000);
   //servoY.attach(servoPinY, 750, 2250);
@@ -26,23 +37,37 @@ void setup() {
   gimbleB.write(gimbleUPosX);
   gimbleU.write(gimbleUPosY);
 
-    Serial1.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-    char input = Serial1.read();
+    char input = Serial.read();
     switch(input){
       case 'w': 
         thrust += 5;
-        liftMotor.write(thrust);
-          Serial1.println(thrust,DEC);
-
+        thrustMotor.write(thrust);
+        Serial.println(thrust,DEC);
       break;
       case 's':
         thrust -= 5;
-        liftMotor.write(thrust);
-          Serial1.println(thrust,DEC);
-
+        thrustMotor.write(thrust);
+        Serial.println(thrust,DEC);
+      break;
+      case 'd':
+        steer -= 5;
+        steering.write(steer);
+      break;
+      case 'a':
+        steer += 5;
+        steering.write(steer);
+      break;
+      case 'q':
+        lift += 5;
+        liftMotor.write(lift);
+      break;
+      case 'z':
+        lift -= 5;
+        liftMotor.write(lift);
       break;
   }
 }
